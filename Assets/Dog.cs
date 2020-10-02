c0a68419ef0722a339814454b48443a94c00cf02
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2020-10-02 19:22:59 -0400

    big chungus push

---
+++ Assets/Dog.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Dog : MonoBehaviour
+{
+    //from https://www.youtube.com/watch?v=jvtFUfJ6CP8&t=301s
+
+    public Transform myLocation;
+
+    public Transform playerTarget;
+    public Transform[] testTargets;
+
+    public Transform currTarget;
+
+    public float speed;
+    public float nextWaypointDistance = 3f;
+    public float spawnPoopTime;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    public Seeker seeker;
+    public Rigidbody2D rb;
+
+    //public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(rb.position, currTarget.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+    }
+}
